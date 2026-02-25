25bcd9c0dea1747dc416624fa928e02d82d325ef
Phurinchai <1166105051088@mail.rmutt.ac.th>
2026-02-25 10:40:54 +0700

    Add files via upload

--- multiple_classification.py
+++ multiple_classification.py
@@ -12,10 +12,44 @@ from streamlit_option_menu import option_menu
 #import model
 loan_model = pickle.load(open("loan_model.sav",'rb'))
 ridingmower_model = pickle.load(open("RidingMowers_model.sav",'rb'))
-
+stress_model = picke.locd(open("stress_model.sav",'rb'))
 
 with st.sidebar:
-    selected = option_menu('Classification',['LOAN','RidingMower'])
+    selected = option_menu('Classification',['LOAN','RidingMower','stress'])
+
+    if(selected == 'Stress'):
+    st.title('Stress Prediction')
+             Age = st.text_input('Age')
+             Gender = st.text_input('Gender')
+             Occupation = st.text_input('Occupation')
+             Device_Type = st.text_input('Device_Type')
+             Daily_Phone_Hours = st.text_input('Daily_Phone_Hours')
+             Social_Media_Hours = st.text_input('Social_Media_Hours')
+             Work_Productivity_Score = st.text_input('Work_Productivity_Score')
+             Sleep_Hours = st.text_input('Sleep_Hours')
+             AApp_Usage_Countge = st.text_input('App_Usage_Count')
+             Caffeine_Intake_Cups = st.text_input('Caffeine_Intake_Cups')
+             Weekend_Screen_Time_Hours = st.text_input('Weekend_Screen_Time_Hours')
+
+    stress_predict =''
+    
+    if st.button('Predict'):
+        stress_predict = stress_predict =''.predict([[
+            float(Age),
+            float(Gender),
+            float(Occupation),
+            float(Device_Type),
+            float(Daily_Phone_Hours),
+            float(Social_Media_Hours),
+            float(Work_Productivity_Score),
+            float(Sleep_Hours),
+            float(AApp_Usage_Countge),
+            float(Caffeine_Intake_Cups),
+            float(Weekend_Screen_Time_Hours),
+        ]])
+        
+    st.success(stress_predict)
+
 
 if(selected == 'RidingMower'):
     st.title('RidingMower Prediction')
